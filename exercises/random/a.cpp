5e90c397ad2cc58ef74f725064873b8754212e1c
Vitor <vitorprofissionalpp10@gmail.com>
2026-06-02 12:31:38 -0300

    a

--- exercises/random/a.cpp
+++ exercises/random/a.cpp
@@ -11,15 +11,6 @@ using namespace std;
 
 int main() {
     fastio;
-    map<char,int> m={{'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}};
-    string ss;
-    int s = 0;
-    cin >> ss;
-    for(int i = 0; i < ss.size(); i++)
-      if(i+1 < ss.size() && m[ss[i]] < m[ss[i+1]]) 
-        s -= m[ss[i]];
-      else 
-        s += m[ss[i]];
-    cout << s << endl;
+    // code goes here
     return 0;
 }
