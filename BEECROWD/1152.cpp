ace93114bf1707859cd471bed84ab39805249c94
Vitor <vitorprofissionalpp10@gmail.com>
2026-07-30 22:56:01 -0300

    new exercises

---
+++ BEECROWD/1152.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define fastio \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+struct edge {
+    int u,v,w;
+};
+
+bool compareEdge(edge a, edge b) {
+    return a.w < b.w;
+}
+
+vector<int> boss;
+
+int find(int x) {
+    if(boss[x] == x) return x;
+    return boss[x] = find(boss[x]);
+}
+
+void join(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if(a != b) boss[a] = b;
+}
+
+int main() {
+    fastio;
+    int n,m;
+    cin >> n >> m;
+    while(n != 0 && m != 0) {
+        ll tc = 0; 
+        vector<edge> edges;
+        for(int i = 0; i < n; i++) {
+            int u,v,w;
+            cin >> u >> v >> w;
+            edges.push_back({u,v,w});
+            tc += w;
+        }
+        sort(edges.begin(), edges.end(), compareEdge);
+        boss.resize(n+1);
+        for(int i = 0; i < n; i++) boss[i] = i;
+        int c = 0;
+        int by = 0;
+        for(edge& Edge : edges) {
+            if(find(Edge.u) != find(Edge.v)) {
+                join(Edge.u, Edge.v);
+                c += Edge.w;
+                by++;
+                if(by == n-1) break;
+            }
+        }
+        cout << tc - c << endl;
+        cin >> n >> m;
+    }
+    return 0;
+}
