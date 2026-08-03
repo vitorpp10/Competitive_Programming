cdea9fe507cddfec88d21711da14520a97eebb59
Vitor <vitorprofissionalpp10@gmail.com>
2026-08-03 13:56:53 -0300

    implementation

--- random/a.cpp
+++ random/a.cpp
@@ -9,32 +9,15 @@
 
 using namespace std;
 
-int main() {     
-    fastio;    
+int main() {
+    fastio;
+    //n->input
+    //x && y where x and y are a compost number, i.e., have some divisors in addition 1 or him 
+    //example: n = 12;
+    //output: x = 4, because 4 is not prime, soon, is a compost number | y = 8 because is a compost number
     int n;
     cin >> n;
-    vector<int> v(n);
-    for(int i = 0; i < n; i++) cin >> v[i];
-    int s = 0, d = 0;
-    int esquerda = 0;
-    int direita = n - 1;
-    bool turno_sereja = true;
-    while (esquerda <= direita) {
-        int carta_escolhida;
-        if (v[esquerda] > v[direita]) {
-            carta_escolhida = v[esquerda];
-            esquerda++; 
-        } else {
-            carta_escolhida = v[direita];
-            direita--;
-        }
-        if (turno_sereja) {
-            s += carta_escolhida;
-        } else {
-            d += carta_escolhida;
-        }
-        turno_sereja = !turno_sereja;
-    }
-    cout << s << " " << d << endl;
+    if((n & 1) == 0) cout << 4 << " " << n - 4 << endl; //the smallest even compost number is 4, soon, n - 4 = another compost number
+    else cout << 9 << " " << n - 9 << endl; //the smallest odd compost number is 9, soon, n - 9 = another compost number
     return 0;
 }
