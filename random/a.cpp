ad139cf0dfa5f18d845398b03db9232869edb5f0
Vitor <vitorpp10@github.com>
2026-08-04 14:28:43 -0300

    debbugs and exec

--- random/a.cpp
+++ random/a.cpp
@@ -11,13 +11,16 @@ using namespace std;
 
 int main() {
     fastio;
-    //n->input
-    //x && y where x and y are a compost number, i.e., have some divisors in addition 1 or him 
-    //example: n = 12;
-    //output: x = 4, because 4 is not prime, soon, is a compost number | y = 8 because is a compost number
-    int n;
-    cin >> n;
-    if((n & 1) == 0) cout << 4 << " " << n - 4 << endl; //the smallest even compost number is 4, soon, n - 4 = another compost number
-    else cout << 9 << " " << n - 9 << endl; //the smallest odd compost number is 9, soon, n - 9 = another compost number
+    int tt;
+    cin >> tt;
+    while(tt--) {
+        int n;
+        bool p = true;
+        cin >> n;
+        int sn = (int)sqrt(n);
+        for(int i = 2; i * i <= sn; i++) if(sn % i == 0) p = false;
+        if(p) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
     return 0;
 }
