948f4314a7bf81291f25d5633068ef936c830d35
Vitor <vitorpp10@github.com>
2026-08-10 13:58:39 -0300

    default changes

--- random/a.cpp
+++ random/a.cpp
@@ -9,26 +9,39 @@
 
 using namespace std;
 
+int dx[]={-1,1,0,0};
+int dy[]={0,0,-1,1};
+
 int main() {
     fastio;
-    //1 a n | v[n + 1]
-    //fa > fb --> fa supervisor > fb 
-    //fa com fa --> sempre superioridade
-    //output = número mínimo de grupos formados
-    //pi = denota o superior imediato do funcionário atual 
-    //pi == -1 --> o funcionário atual não tem supervisor
-    //pi != i, ou seja, ninguém é supervisor de si mesmo 
-    //grupo -> funconarios e somente 1 chefe(supervisor)
-    //while(true) {
-    //  if(vetor[atual] = -1) {
-    //      c++;
-    //      break;
-    //  } else {
-    //      vetor[atual] = atual;
-    //  }
-    //
-    //}
-    int n;
-    cin >> n;
+    int n,m;
+    cin >> n >> m;
+    vector<vector<char>> v(n, vector<char>(m));
+    for(int i = 0; i < n; i++) 
+        for(int j = 0; j < m; j++) cin >> v[i][j];
+    queue<pair<int,int>> q;
+    int c = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(v[i][j] == '.') {
+                v[i][j] = '#';
+                c++;
+                q.push({i,j});
+                while(q.size() > 0) {
+                    pair<int,int> at = q.front();
+                    q.pop();
+                    for(int k = 0; k < 4; k++) {
+                        int nx = at.first + dx[k];
+                        int ny = at.second + dy[k];
+                        if(nx >= 0 && ny >= 0 && nx < v.size() && ny < v[0].size() && v[nx][ny] == '.') {
+                            v[nx][ny] = '#';
+                            q.push({nx,ny});
+                        } 
+                    }
+                }
+            }
+        }
+    }
+    cout << c << endl;
     return 0;
 }
