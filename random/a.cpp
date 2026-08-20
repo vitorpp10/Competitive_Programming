00c0ca37de6c0572f963b6041d2a56fd7afc86f4
Vitor <vitorpp10@github.com>
2026-08-20 11:49:23 -0300

    2 more exercises randoms

--- random/a.cpp
+++ random/a.cpp
@@ -11,12 +11,6 @@ using namespace std;
 
 int main() {
     fastio;
-    int n, k;
-    cin >> n >> k;
-    vector<int> ct(n+1);
-    vector<int> dp(n+1);
-    for(int i = 1; i <= n; i++) cin >> ct[i];
-    dp[1] = 0;
-    dp[2] = abs(ct[1] - )
+
     return 0;
 }
