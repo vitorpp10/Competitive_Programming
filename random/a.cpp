781a71feba0bfdec73f91c159ce5cbcac1f87032
Vitor <vitorpp10@github.com>
2026-08-20 10:36:27 -0300

    normal

--- random/a.cpp
+++ random/a.cpp
@@ -11,18 +11,12 @@ using namespace std;
 
 int main() {
     fastio;
-    int n;
-    cin >> n;
+    int n, k;
+    cin >> n >> k;
     vector<int> ct(n+1);
     vector<int> dp(n+1);
     for(int i = 1; i <= n; i++) cin >> ct[i];
     dp[1] = 0;
-    dp[2] = abs(ct[2] - ct[1]);
-    for(int i = 3; i <= n ; i++) {
-        int p1 = dp[i-1] + abs(ct[i] - ct[i - 1]);
-        int p2 = dp[i-2] + abs(ct[i] - ct[i - 2]); 
-        dp[i] = min(p1, p2);
-    }
-    cout << dp[n] << endl;
+    dp[2] = abs(ct[1] - )
     return 0;
 }
