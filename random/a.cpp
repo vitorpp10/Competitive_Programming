7a5eba7261f3f4816cdeb0977c33cf4802e4c7ad
Vitor <vitorpp10@github.com>
2026-09-03 13:34:34 -0300

    normal

--- random/a.cpp
+++ random/a.cpp
@@ -11,49 +11,38 @@ using namespace std;
 
 const ll INF = 1e18;
 
-vector<int> dj(int o, vector<vector<pair<int,int>>>& g) {
-    vector<ll> d(g.size(), INF);
-    vector<int> p(g.size(), -1);
-    priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> q;
-    d[o] = 0;
-    q.push({0, o});
-    while(!q.empty()) {
-        auto [w, cw] = q.top();
-        q.pop();
-        if(w > d[cw]) continue;
-        for(auto [v, cv] : g[cw]) {
-            if(d[cv] > d[cw] + v) {
-                d[cv] = d[cw] + v; 
-                p[cv] = cw;
-                q.push({d[cv], cv});
-            }
-        }
-    }
-    return p;
-}
-
 int main() {
     fastio;
     int n,m;
     cin >> n >> m;
-    vector<vector<pair<int,int>>> g(n+1);
+    vector<vector<pair<int,int>>> g(n);
     for(int i = 0; i < m; i++) {
         int u,v,l;
         cin >> u >> v >> l;
         g[u].push_back({l,v});
-        g[u].push_back({l,u});
     }
-    vector<int> r = dj(0, g);
-    if(r[n] == -1) cout << "IMPOSSIBLE" << endl;
-    else {
-        vector<int> rr;
-        int t = n;
-        while(t != -1) {
-            rr.push_back(t);
-            t = r[t];
+    vector<vector<ll>> d(n + 1, vector<ll>(2, INF));
+    using state = tuple<ll, int, int>;
+    priority_queue<state, vector<state>, greater<state>> q;
+    d[0][0] = 0;
+    q.push({0, 1, 0});
+    while(q.size() > 0) {
+        auto [cost, city, coupon] = q.top();
+        q.pop();
+        if(cost > d[city][coupon]) continue;
+        for(auto [kg, neigh] : g[city]) {
+            if(d[neigh][coupon] > cost + kg) {
+                d[neigh][coupon] = cost + kg;
+                q.push({d[neigh][coupon], neigh, coupon});
+            }
+            if(coupon == 0) {
+                if(d[neigh][1] > cost + (kg / 2)) {
+                    d[neigh][1] = cost + (kg / 2);
+                    q.push({d[neigh][1], neigh, 1});
+                }
+            }
         }
-        reverse(rr.begin(), rr.end());
-        for(int& rrr : rr) cout << rrr << endl;
     }
+    cout << d[n][1] << endl;
     return 0;
 }
